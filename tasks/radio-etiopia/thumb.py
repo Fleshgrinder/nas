6fd799a1ae4397630c2a1db8a6dc6b8ac7bcdaef
Fleshgrinder <fleshgrinder@users.noreply.github.com>
2020-03-27 17:33:27 +0100

    Filter dirs

--- tasks/radio-etiopia/thumb.py
+++ tasks/radio-etiopia/thumb.py
@@ -5,6 +5,6 @@ from pathlib import Path
 from PIL import Image
 
 for path in Path('/out').rglob('folder.jpg'):
-
-    Image.open(path).resize((100, 100), resample=Image.LANCZOS, reducing_gap=3.0) \
-        .save(path.parent / 'thumb.jpg', 'JPEG', optimize=True, quality=85)
+    if path.is_file():
+        Image.open(path).resize((100, 100), resample=Image.LANCZOS, reducing_gap=3.0) \
+            .save(path.parent / 'thumb.jpg', 'JPEG', optimize=True, quality=85)
